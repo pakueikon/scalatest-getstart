8b05aa8d5641bb0ef9af24bea11d31b377e49d58
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-02-09 20:52:16 +0900

    shouldMatchersの追加

--- src/main/scala/jp/com/Calculator.scala
+++ src/main/scala/jp/com/Calculator.scala
@@ -1,5 +1,19 @@
 package jp.com
 
-class Calculator {
+case class Calculator (calculatedDefault:Int) {
 
+  def this() {
+    this(0)
+  }
+
+  var calculated = calculatedDefault
+  var addedHistory:List[Int] = List()
+
+  def add(x:Int):Int = {
+    addedHistory = x :: addedHistory
+    calculated += x
+    calculated
+  }
+
+  override def toString:String = "Calculated History : " + addedHistory.mkString(", ")
 }
--- src/test/scala/jp/com/Calculator.scala
+++ src/test/scala/jp/com/Calculator.scala
@@ -1,9 +1,61 @@
 package jp.com
 
-import org.scalatest.FunSuite
+import org.scalatest.{ShouldMatchers, FunSuite}
 
-class CalculatorTest extends FunSuite {
-  test("An empty Set should have size 0") {
-    assert(Set.empty.size == 0)
+/**
+ * xUnit形式のテスト
+ * http://www.scalatest.org/user_guide/selecting_a_style
+ * におけるFunSuite
+ */
+class CalculatorTest extends FunSuite with ShouldMatchers{
+
+  /**
+   * 普通のUnitテスト
+   */
+  test("add method returns added value with assertion") {
+    val c = Calculator(0)
+    assert(c.add(10) == 10)
+    assert(c.calculated ==10)
+  }
+
+  /**
+   * trait ShouldMatchersを利用したUnitテスト
+   */
+  test("add method returns added value with should matchers") {
+    val c = Calculator(0)
+    c.add(10) shouldBe (10)
+    c.calculated shouldBe (10)
+  }
+
+  /**
+   * http://www.scalatest.org/user_guide/using_matchers
+   */
+  test("add method returns added value with should matchers on various patterns") {
+    val c = Calculator(0)
+    c.add(10)
+    c.calculated should equal (10)
+    c.calculated should === (10)
+    c.calculated should be (10)
+    c.calculated shouldEqual 10
+    c.calculated shouldBe (10)
+  }
+
+  test("added history size is correct with should matchers") {
+    val c = Calculator(0)
+    c.add(10)
+    c.addedHistory should have size 1
+    c.addedHistory should have length 1
+  }
+
+  test("toString returns correct Message with should matchers") {
+    val c = Calculator(0)
+    c.add(10)
+    c.toString should startWith ("Calculated")
+    c.toString should endWith ("10")
+    c.toString should include ("History")
+
+    c.toString should startWith regex "Calc.lated"
+    c.toString should endWith regex "1."
+    c.toString should include regex ".istory"
   }
 }
